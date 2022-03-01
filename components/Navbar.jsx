3e64db8ceec26c00d340df5a54b9d8b89ec04f5d
RogelioRichmanAstronaut <56854119+RogelioRichmanAstronaut@users.noreply.github.com>
2022-02-28 19:16:33 -0500

    logo
    logo

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         </Link>
         <li className={styles.listItem}>Products</li>
         <li className={styles.listItem}>Menu</li>
-        <Image src="/img/logo.png" alt="logo" width="160px" height="69px" />
+        <Image src="/img/logo.png" objectFit="cover" alt="logo" width="202px" height="134px" />
         <li className={styles.listItem}>Events</li>
         <li className={styles.listItem}>Blog</li>
         <li className={styles.listItem}>Contact</li>
